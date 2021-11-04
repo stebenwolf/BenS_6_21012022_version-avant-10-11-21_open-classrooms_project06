2028d764fd78abf8105d746954e7602d2feef8e3
Stebenwolf <benjamin@the-org.eu>
2021-11-04 11:40:31 +0100

    Create Gallery object

---
+++ js/gallery.ts
@@ -0,0 +1,36 @@
+import { Media } from "./media.js";
+
+
+interface Gallery {
+}
+
+class Gallery {
+    constructor() {
+    }
+
+    createGallery(media: Media) {
+        
+        if(document.querySelector(".gallery")) {
+
+            // on sélectionne le conteneur
+            const gallerySection = document.querySelector(".gallery");
+            
+            // on créé les éléments de la galerie
+            const mediaZone = document.createElement("div");
+            const pictureZone =  document.createElement("picture");
+            const imgZone = document.createElement("img");
+            
+            // on créé les classes et propriétés associées
+            
+            // on intègre les données récupérées
+            imgZone.src = "./img/"+media.photographerId+"/"+media.image;
+            
+            // on attache les éléments au bon endroit
+            gallerySection.append(mediaZone);
+            mediaZone.append(pictureZone);
+            pictureZone.append(imgZone);
+        }
+    }
+}
+
+export {Gallery}
